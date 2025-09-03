d5e12d2903678826574ab03a97b5930e4f517eee
Ruslan Belchenkov <r.belchenkov@litefinance.com>
2025-09-03 15:26:09 +0300

    Fix

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,11 +1,14 @@
 module.exports = {
     apps: [
         {
-            name: 'FoxPrice Test',
+            name: 'FixPrice',
             port: '4206',
-            exec_mode: 'cluster',
-            instances: 'max',
-            script: './.output/server/index.mjs'
+            script: '.output/server/index.mjs',
+            exec_mode: 'fork',
+            instances: 1,
+            watch: false,
+            node_args: ['--max-old-space-size=256'],
+            env: { NODE_ENV: 'production' }
         }
     ]
 }
